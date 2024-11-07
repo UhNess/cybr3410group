0bbb624b459b5a07c6c521fb9f99b312b410d031
UhNess <jokes0607@yahoo.com>
2024-11-07 02:11:01 -0500

    Josh changed Login to fix SQL vulnerability, #noted out original code, and noted what was vulnerable about the original VS what the fix did for the code

--- controllers/sqlController.py
+++ controllers/sqlController.py
@@ -2,10 +2,17 @@ from config.dbConfig import get_db_connection
 
 def login(username, password):
     conn = get_db_connection()
-    sql = f"SELECT * FROM users WHERE username = '{username}' AND password = '{password}'"  # SQL Injection Vulnerability
-    user = conn.execute(sql).fetchone()
+    # sql = f"SELECT * FROM users WHERE username = '{username}' AND password = '{password}'"  # SQL Injection Vulnerability
+    sql = "SELECT * FROM users WHERE username = ? AND password = ?"
+    # user = conn.execute(sql).fetchone()
+    user = conn.execute(sql, (username, password)).fetchone()
     conn.close()
     return user
+    # noted out code is original code, but has the  ' OR '1'='1  for user and any password (ex: password123) for the password vulnerability
+    # making a search into SQL code looking like; SELECT * FROM users WHERE username = '' OR '1'='1' AND password = 'password123'
+    # the code doesnt use prepared statements or parameterized queries, which are designed to separate SQL code from data.
+    # the "?" placeholder is used in the SQL query and any actual values are passed as a tuple.
+
 
 def search(query):
     conn = get_db_connection()
